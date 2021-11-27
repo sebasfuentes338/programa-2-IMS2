87156ca22831e4690b83b282c9437853e09cbb3a
Sebastianfuent6 <90064482+sebasfuentes338@users.noreply.github.com>
2021-11-27 04:16:26 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main() {
+int casa=0;
+int carro=0;
+cout<<” digite un numero”;
+cin>>casa; 
+cout<<” digite un numero”;
+cin>>carro;
+ if (casa<carro); {
+   cout<<casa <<”es mayor” << carro;
+}
+Else {
+ cout<<casa <<”es menor” << carro;
+}
+
+
+}
